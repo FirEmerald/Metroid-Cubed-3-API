d883a33b1fcb007b60136ec8927a48175ffc3fd1
FirEmerald <mccaslinwalt@gmail.com>
2019-02-28 16:34:00 -0600

    1.12.2 preparations
    remove existing API classes, add player_tags.xml

--- src/main/java/metroidcubed3/api/entity/IEntityShip.java
+++
@@ -1,9 +0,0 @@
-package metroidcubed3.api.entity;
-
-/** ONLY USED TO ENSURE THE SHIP IS MOUNTED ON THE CLIENT AFTER TRAVELING BETWEEN PLANETS */
-public interface IEntityShip
-{
-	public void setNeedsMounting();
-	
-	public void setMounted();
-}
